a3f0b8f5ba21851eef453d6d924dbf5b578d895b
doaa04 <doaarigui0@gmail.com>
2024-07-22 13:37:22 +0100

    .

--- src/app/soutien/sommeil/questions/page.jsx
+++ src/app/soutien/sommeil/questions/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Header from '../../../../components/Header';
 import Link from 'next/link';
 import tests from './Sommeil';
