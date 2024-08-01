acac79f271c1ead29bc1c0a32f6cb7cfecb2ce0c
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-08-01 02:32:31 +0100

    fix sume prb

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import createIntlMiddleware from 'next-intl/middleware';
 
 
-const publicRoutes = ["/",'/ar','/fr','/fr/auth','/ar/auth','/auth','/forgotPassword', '/ar/forgotPassword','/fr/forgotPassword', '/fr/register', '/ar/register','/register']
+const publicRoutes = ['/fr','/ar','/','/ar/auth/firstlogin','/auth/firstlogin','/fr/auth/firstlogin','/ar/auth/professionnels','/fr/auth/professionnels','/auth/professionnels','/ar/auth/medecins','/fr/auth/medecins','/auth/medecins','/fr/auth/jeunes','/ar/auth/jeunes','/auth/jeunes','/forgotPassword', '/ar/forgotPassword','/fr/forgotPassword', '/fr/register/jeunes', '/ar/register/jeunes','/register/jeunes',"/fr/register/medecins",'/ar/register/medecins','/register/professionnels','/fr/register/professionnels','/ar/register/professionnels']
 
 // Create the next-intl middleware
 const intlMiddleware = createIntlMiddleware({
@@ -11,18 +11,16 @@ const intlMiddleware = createIntlMiddleware({
 });
 
 export function middleware(request) {
+  
   const { pathname } = request.nextUrl;
   const cookies = request.cookies;
 
-  const isPublicRoute = publicRoutes.some(route => pathname.startsWith(route));
+  const isPublicRoute = publicRoutes.some(route => pathname==route);
 
   if (!isPublicRoute) {
     return NextResponse.next();
   }
 
-  if (pathname.startsWith('/soutien')) {
-    return NextResponse.next();
-  }
 
 
   // Retrieve the NEXT_LOCALE cookie value
@@ -40,9 +38,11 @@ export function middleware(request) {
   // Check if the URL path already includes a locale (e.g., /fr or /ar)
   const isLocaleInPath = /^\/(fr|ar)(\/|$)/.test(pathname);
 
+  
   if (!isLocaleInPath) {
     // If no locale is in the path, prepend the locale from the cookie or default to 'fr'
     const url = request.nextUrl.clone();
+    
     url.pathname = `/${validLocale}${pathname}`;
     return NextResponse.redirect(url);
   }
